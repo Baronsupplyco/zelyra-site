d8e07187ca30ce4095ee953c44b75adea375b474
Zelyra <andre-martimiano@hotmail.com>
2025-09-27 18:15:02 -0300

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const menuToggle = document.getElementById('menuToggle');
-const menu = document.getElementById('menu');
-menuToggle?.addEventListener('click', () => {
-  menu.classList.toggle('open');
-});
